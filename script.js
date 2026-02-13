424b7f0be7c303fec5f1a861c02b5406b479ab38
Portoks <vpanajotovic835@gmail.com>
2026-02-13 16:51:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 const gifStages = [
-    "https://media.tenor.com/EBV7OT7ACfwAAAAj/u-u-qua-qua-u-quaa.gif",    // 0 normal
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif",  // 1 confused
-    "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",             // 2 pleading
-    "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
-    "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder
-    "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",             // 5 devastated
-    "https://media.tenor.com/5_tv1HquZlcAAAAj/chiikawa.gif",               // 6 very devastated
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAC/chiikawa-hachiware.gif"  // 7 crying runaway
+    "https://media1.tenor.com/m/eT26MX-fclwAAAAd/spike-cowboy-bebop.gif",    // 0 normal
+    "https://media1.tenor.com/m/adWY41jtMRQAAAAC/scratching-head-thinking.gif",  // 1 confused
+    "https://media1.tenor.com/m/TL5Hz2WVxgsAAAAC/spike-spiegel-crying.gif",             // 2 pleading
+    "https://media1.tenor.com/m/ffy6l4UkyEQAAAAd/cowboy-bebop-spike-spiegel.gif",             // 3 sad
+    "https://media1.tenor.com/m/Xqnsa2YJgKcAAAAC/cowboy-bebop-spike-spiegel.gif",       // 4 sadder
+    "https://media1.tenor.com/m/KcOKR1GVo30AAAAC/cowboy-bebop-faye.gif",             // 5 devastated
+    "https://media1.tenor.com/m/IBe1N6OqVrMAAAAd/spike-cowboy.gif",               // 6 very devastated
+    "https://media1.tenor.com/m/fFSln2_V98IAAAAd/shioshishio-muscle-sunglasses.gif"  // 7 crying runaway
 ]
 
 const noMessages = [
     "No",
-    "Are you positive? 🤔",
-    "Pookie please... 🥺",
+    "Jel si sigurna? 🤔",
+    "Ljubav please... 🥺",
     "If you say no, I will be really sad...",
     "I will be very sad... 😢",
     "Please??? 💔",
     "Don't do this to me...",
-    "Last chance! 😭",
-    "You can't catch me anyway 😜"
+    "Poslednja sansa! 😭",
+    "Ne mozes da me uvatis 😜"
 ]
 
 const yesTeasePokes = [
